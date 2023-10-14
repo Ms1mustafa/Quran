8b861e694c78865d665c69933c5111cd09b9a07b
Mustafa <mustafaalasadi58@gmail.com>
2023-10-15 00:57:35 +0300

    Update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 async function getQuranAudioInfo() {
   try {
     const response = await fetch(
-      "https://api.alquran.cloud/v1/quran/ar.alafasy"
+      "http://api.alquran.cloud/v1/quran/ar.alafasy"
     );
 
     if (!response.ok) {
